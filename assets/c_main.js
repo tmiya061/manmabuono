70d44248a9b40ac8326b3c1206530580336368d9
Ryo <bookcastle1994@gmail.com>
2024-02-11 11:30:16 +0900

    top coding

--- assets/c_main.js
+++ assets/c_main.js
@@ -59,4 +59,42 @@ if ( window.document.body.id === 'top' ) {
         slidesPerView: 1, // 一度に表示する枚数
         spaceBetween: 30,
     });
+
+    const topPicture = new Swiper('.swiper.--topPicture', { //名前を変える
+        loop: true, //最後→最初に戻るループ再生を有効に
+        autoplay: { 
+            delay: 0, //何秒ごとにスライドを動かすか
+        },
+        speed: 5000, //表示切り替えのスピード
+        effect: "slide", //切り替えのmotion (※1)
+        centeredSlides: true, //中央寄せ
+        allowTouchMove: true, // スワイプで表示の切り替えを無効に
+        slidesPerView: "auto",
+        spaceBetween: 0,
+        // breakpoints: { //画面幅による表示枚数と余白の指定
+        //     320: {
+        //         slidesPerView: 1,
+        //         spaceBetween: 0,
+        //     },
+        //     375: {
+        //         slidesPerView: 1.4,
+        //         spaceBetween: 0,
+        //     },
+        //     750: {
+        //         slidesPerView: 2.5,
+        //         spaceBetween: 0,
+        //     },
+        //     1025: {
+        //         slidesPerView: 3.4,
+        //         spaceBetween: 0,
+        //     },
+        //     1300: {
+        //         slidesPerView: 3.8,
+        //         spaceBetween: 0,
+        //     },
+        //     1500: {
+        //         slidesPerView: 4,
+        //     },
+        // }
+    });
 }
